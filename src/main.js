4483027448fc89a591d8053bfaad27bd5fd44b29
Michael Burton <micbur1@gmail.com>
2021-04-21 14:04:16 -0700

    add functonality for submit button to display pic of day
    Co-authored-by: Vanessa Su <vnessa.su@gmail.com>
    Co-authored-by: Isaac Moreno <ipdxcreative@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,13 +2,22 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import PicOfDay from "picOfDay.js"
+import PicOfDay from "./picOfDay.js";
 
 $(document).ready(function () {
   $("#picBtn").click(function () {
     let inputtedDate = $("#inputtedDate").val();
 
-    inputtedDate()
-  })
-})
+    $("#show").text(inputtedDate);
+
+    let picPromise = PicOfDay.getPic(inputtedDate);
+    picPromise.then(function (response) {
+      const body = JSON.parse(response);
+      $("#showPic").html(`<img src="${body.url}"/>`);
+    }, function (error) {
+      console.error("Request error: ", error);
+    });
+
+  });
+});
 
--- src/picOfDay.js
+++ src/picOfDay.js
@@ -1,21 +1,19 @@
 export default class PicOfDay {
-  static PicOfDay(date) {
+  static getPic(date) {
     return new Promise(function (resolve, reject) {
       const apiKey = process.env.API_KEY;
-
       let request = new XMLHttpRequest();
       const url = `https://api.nasa.gov/planetary/apod?date=${date}&api_key=${apiKey}`;
 
-      request.onLoad = function () {
+      request.onload = function () { //onload is lowercase @.@
         if (this.status === 200) {
           resolve(request.response);
         } else {
           reject(request.response);
         }
-      }
+      };
       request.open("GET", url, true); // "GET" is type of request, url api call, true determines if its async
       request.send();
-    })
+    });
   }
-
 }
