81efea8f988eb51525a0d76f138947e7b3e15262
Michael Burton <micbur1@gmail.com>
2021-04-21 11:47:23 -0700

    WIP add click functionality to submit button to show the picture of the day.
    Co-authored-by: Vanessa Su <vnessa.su@gmail.com>
    Co-authored-by: Isaac Moreno <ipdxcreative@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import PicOfDay from "picOfDay.js"
 
+$(document).ready(function () {
+  $("#picBtn").click(function () {
+    let inputtedDate = $("#inputtedDate").val();
 
+    inputtedDate()
+  })
+})
 
